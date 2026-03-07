1fc3af922abc75ae19683c07d0cc98eab28b4c6c
gdb22 <gdb22@njit.edu>
2026-03-06 23:41:20 -0500

    more changes for deployment link

--- student-reality-lab-folder/src/App.tsx
+++ student-reality-lab-folder/src/App.tsx
@@ -32,20 +32,20 @@ function App() {
       <section className="story">
         <h3>Story</h3>
         <p>
-          This line chart shows a steady increase in average student loan balance
+          The first line chart shows a steady increase in average student loan balance 
           over the past twenty years. Although there were some fluctuations
           throughout the years there is a consistent increase. With this
           substantial increase, it raises a question whether student loans will
           increase throughout the next couple of years. If it does then it would
           negatively impact future college students who want to seek an
           affordable education. In the last twenty years student loans have
-          risen $18,000, imagine the increase in the next twenty years. This
+          risen $18,000. Imagine the increase in the next twenty years. This
           shows why this can be a potential problem in the future. It is very
-          important that the rate of student loans stabilize or at least
-          decrease. The financial strain current college students face can
-          already be difficult to manage. This chart helps spread awareness
-          about the student loan crisis and how it affects many college students.
-          Being educated on this topic can help future students better
+          important that the rate of student loans stabilize in the long term. Otherwise,
+          it can negatively impact future students. The financial strain current college 
+          students face can already be difficult to manage. Both of these charts help 
+          spread awareness about the student loan crisis and how it affects many college 
+          students. Being educated on this topic can help future students better
           understand the potential consequences of taking student loans.
         </p>
       </section>
